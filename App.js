180fa81971ae4d614b4583e824b7ff3eb06b3dbf
NB <n@batten.cc>
2024-03-29 22:44:05 -0400

    Adds in the components folder and creates MissionTopBar.js, MissionDetail.js, and setup.js

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
+import React, { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as ScreenOrienatation from 'expo-screen-orientation';
+import MissionTopBar from './components/MissionTopBar';
+// import Orientation from 'react-native-orientation';
 
 export default function App() {
+  // Locking the app in landscape orientation, should only be needed in dev testing as once it is compiled to an APK the same
+  // can be achieved by modifying the AndroidManifest.xml file
+  const [orientation, setOrientation] = useState(3);
+
+  useEffect(() => {
+    lockOrientation();
+  }, []);
+
+  const lockOrientation = async () => {
+    await ScreenOrienatation.lockAsync(
+      ScreenOrienatation.OrientationLock.LANDSCAPE_RIGHT
+    );
+    const o = await ScreenOrienatation.getOrientationAsync();
+    setOrientation(o);
+  }
+
+
   return (
-    <View style={styles.container}>
-      <Text></Text>
+    <View style={[styles.container, {flexDirection: "column"}]}>
+      <MissionTopBar/>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +34,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#1f1f2e',
     alignItems: 'center',
     justifyContent: 'center',
+
   },
 });
---
+++ components/MissionTopBar.js
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+const MissionTopBar = () => {
+
+    const [ dispMessage, setDispMessage ] = useState(false);
+
+    const message = () => {
+        return (
+            <View style={{backgroundColor: "orange"}}>
+                <Text>You pressed the number button!</Text>
+            </View>
+        )
+    }
+
+    const handlePress = () => {
+        dispMessage ? setDispMessage(false) : setDispMessage(true);
+    }
+
+    return (
+        <View>
+            <Button
+                onPress={handlePress}
+                title="This is a button"
+                color="blue"
+            />
+            {dispMessage ? message() : null}
+        </View>
+    )
+}
+
+export default MissionTopBar;
